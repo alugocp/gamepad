7a5388c0bd3300b83cfbdeb5acd15ead94df8133
Alex Lugo <alugocp@aim.com>
2025-01-05 22:10:23 -0500

    It controls the mouse now

--- switch/switch.c
+++ switch/switch.c
@@ -19,11 +19,14 @@ MODULE_LICENSE("GPL");
 // Global variables
 static unsigned char *gamepad_buffer;
 static struct usb_device *device;
+static struct input_dev *idev;
 static struct urb *irq;
+static int mouse_down = 0;
 
 // Reads some data from the gamepad
-static void gamepad_irq(struct urb *urb) {
-    unsigned int a;
+static void gamepad_urb_complete(struct urb *urb) {
+    int dx;
+    int dy;
     switch (urb->status) {
         case 0:
             break;
@@ -35,12 +38,26 @@ static void gamepad_irq(struct urb *urb) {
             usb_submit_urb(irq, GFP_ATOMIC);
             return;
     }
-    printk(KERN_INFO "Gamepad says:");
-    for (a = 0; a < BUFFER_SIZE; a++) {
-        printk(KERN_INFO " 0x%x", gamepad_buffer[a]);
-    }
-    printk(KERN_INFO "\n");
 
+    // Business logic to handle gamepad buffer data
+    if (gamepad_buffer[0] == 4) {
+        if (!mouse_down) {
+            printk(KERN_INFO "Down!\n");
+            input_report_key(idev, BTN_LEFT, 1);
+            mouse_down = 1;
+        }
+    } else if (mouse_down) {
+        printk(KERN_INFO "Up!\n");
+        input_report_key(idev, BTN_LEFT, 0);
+        mouse_down = 0;
+    }
+    dx = (int)((gamepad_buffer[3] - 128) / 16);
+    dy = (int)((gamepad_buffer[4] - 128) / 16);
+    input_report_rel(idev, REL_X, dx);
+    input_report_rel(idev, REL_Y, dy);
+    input_sync(idev);
+
+    // Resubmit URB so we keep polling
     if (usb_submit_urb(irq, GFP_ATOMIC)) {
         printk(KERN_ERR "Could not resubmit URB\n");
     }
@@ -52,16 +69,28 @@ static int gamepad_probe(struct usb_interface *interface, const struct usb_devic
     int result;
     dma_addr_t data_dma;
     printk(KERN_INFO "Gamepad device detected\n");
+
+    // Allocate and register the input device (controls the mouse)
+    idev = input_allocate_device();
+    idev->name = "gamepad_mouse";
+    set_bit(EV_KEY, idev->evbit);
+    set_bit(BTN_LEFT, idev->keybit);
+    set_bit(EV_REL, idev->evbit);
+    set_bit(REL_X, idev->relbit);
+    set_bit(REL_Y, idev->relbit);
+    if (input_register_device(idev)) {
+        printk(KERN_ERR "Could not register input device\n");
+    }
+
+    // Initialize logic to capture USB input stream data
     device = interface_to_usbdev(interface);
     irq = usb_alloc_urb(0, GFP_KERNEL);
     if (!irq) {
         printk(KERN_ERR "Could not allocate a URB\n");
     }
-
-    // Initialize logic for gamepad_irq function
     pipe = usb_rcvintpipe(device, ADDRESS);
     gamepad_buffer = usb_alloc_coherent(device, BUFFER_SIZE, GFP_KERNEL, &data_dma);
-    usb_fill_int_urb(irq, device, pipe, gamepad_buffer, BUFFER_SIZE, gamepad_irq, NULL, INTERVAL);
+    usb_fill_int_urb(irq, device, pipe, gamepad_buffer, BUFFER_SIZE, gamepad_urb_complete, NULL, INTERVAL);
 
     // Kick off the URB submission cycle
     result = usb_submit_urb(irq, GFP_KERNEL);
@@ -74,6 +103,7 @@ static int gamepad_probe(struct usb_interface *interface, const struct usb_devic
 // Called when our device is disconnected
 static void gamepad_disconnect(struct usb_interface *interface) {
     printk(KERN_INFO "Gamepad device removed\n");
+    input_unregister_device(idev);
 }
 
 // Table that lists the vendor/product IDs this driver supports
