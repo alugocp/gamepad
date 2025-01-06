559b3cb10f338771d78e955cc966a0ac4b5b8c54
Alex Lugo <alugocp@aim.com>
2025-01-05 20:59:05 -0500

    Finally reads stream from controller

--- switch/switch.c
+++ switch/switch.c
@@ -7,6 +7,7 @@
 #include <linux/kernel.h>
 #include <linux/usb.h>
 #include <linux/usb/input.h>
+#define BUFFER_SIZE 8
 #define ADDRESS 0x81
 #define INTERVAL 8
 
@@ -16,9 +17,8 @@ MODULE_AUTHOR("Alex Lugo <alugocp@aim.com>");
 MODULE_LICENSE("GPL");
 
 // Global variables
-static unsigned char gamepad_buffer[8];
+static unsigned char *gamepad_buffer;
 static struct usb_device *device;
-static struct input_dev *input;
 static struct urb *irq;
 
 // Reads some data from the gamepad
@@ -36,27 +36,21 @@ static void gamepad_irq(struct urb *urb) {
             return;
     }
     printk(KERN_INFO "Gamepad says:");
-    for (a = 0; a < 8; a++) {
+    for (a = 0; a < BUFFER_SIZE; a++) {
         printk(KERN_INFO " 0x%x", gamepad_buffer[a]);
     }
     printk(KERN_INFO "\n");
-}
 
-// Called when the gamepad buffer is opened
-static int gamepad_open(struct input_dev *dev) {
-    return usb_submit_urb(irq, GFP_KERNEL) ? -EIO : 0;
-}
-
-// Called when the gamepad buffer is closed
-static void gamepad_close(struct input_dev *dev) {
-    usb_kill_urb(irq);
+    if (usb_submit_urb(irq, GFP_ATOMIC)) {
+        printk(KERN_ERR "Could not resubmit URB\n");
+    }
 }
 
 // Probe function that gets called when our device is connected
 static int gamepad_probe(struct usb_interface *interface, const struct usb_device_id *id) {
     int pipe;
-    int maxp;
     int result;
+    dma_addr_t data_dma;
     printk(KERN_INFO "Gamepad device detected\n");
     device = interface_to_usbdev(interface);
     irq = usb_alloc_urb(0, GFP_KERNEL);
@@ -64,23 +58,15 @@ static int gamepad_probe(struct usb_interface *interface, const struct usb_devic
         printk(KERN_ERR "Could not allocate a URB\n");
     }
 
-    // Set up the input device
-    input = input_allocate_device();
-    input->name = "usb/switch";
-    input->dev.parent = &interface->dev;
-    input->open = gamepad_open;
-    input->close = gamepad_close;
-    irq->dev = device;
-
     // Initialize logic for gamepad_irq function
     pipe = usb_rcvintpipe(device, ADDRESS);
-    maxp = usb_maxpacket(device, pipe, false);
-    usb_fill_int_urb(irq, device, pipe, gamepad_buffer, (maxp > 8 ? 8 : maxp), gamepad_irq, NULL, INTERVAL);
+    gamepad_buffer = usb_alloc_coherent(device, BUFFER_SIZE, GFP_KERNEL, &data_dma);
+    usb_fill_int_urb(irq, device, pipe, gamepad_buffer, BUFFER_SIZE, gamepad_irq, NULL, INTERVAL);
 
-    // Register the input device
-    result = input_register_device(input);
+    // Kick off the URB submission cycle
+    result = usb_submit_urb(irq, GFP_KERNEL);
     if (result) {
-        printk(KERN_ERR "Could not register the USB gamepad\n");
+        printk(KERN_ERR "Could not submit URB\n");
     }
     return result;
 }
@@ -88,20 +74,17 @@ static int gamepad_probe(struct usb_interface *interface, const struct usb_devic
 // Called when our device is disconnected
 static void gamepad_disconnect(struct usb_interface *interface) {
     printk(KERN_INFO "Gamepad device removed\n");
-    input_unregister_device(input);
 }
 
 // Table that lists the vendor/product IDs this driver supports
-static struct usb_device_id gamepad_table[] =
-{
+static struct usb_device_id gamepad_table[] = {
     { USB_DEVICE(0x20d6, 0xa713) },
     {}
 };
 MODULE_DEVICE_TABLE (usb, gamepad_table);
 
 // Table that defines probe/disconnect functions
-static struct usb_driver gamepad_driver =
-{
+static struct usb_driver gamepad_driver = {
     .name = "gamepad_driver",
     .id_table = gamepad_table,
     .probe = gamepad_probe,
